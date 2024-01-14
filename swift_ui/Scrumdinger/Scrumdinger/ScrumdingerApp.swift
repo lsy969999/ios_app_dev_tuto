b3b64f50bfe2e9d31fec51e4eaf8763da2424a75
lsy969999 <lsy969999@gmail.com>
2024-01-14 15:35:39 +0900

    swift ui scrumbling init

---
+++ swift_ui/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by SY L on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
