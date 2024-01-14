5beb9e807969984ccbb3fd4b37ebf06e8812d63e
lsy969999 <lsy969999@gmail.com>
2024-01-14 18:27:43 +0900

    list identifier

---
+++ swift_ui/Scrumdinger/Scrumdinger/ScrumView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumView.swift
+//  Scrumdinger
+//
+//  Created by SY L on 1/14/24.
+//
+
+import SwiftUI
+
+struct ScrumView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumView()
+}
